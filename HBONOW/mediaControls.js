6656523860555d4ebabd742cdfaac508cba4609a
Logan Gibson <lgibso34@uncc.edu>
2019-05-16 12:10:23 -0400

    first push

---
+++ HBONOW/mediaControls.js
@@ -0,0 +1,19 @@
+//https://github.com/carlin-q-scott/browser-media-keys/blob/master/lib/firefoxHotkeys.js
+
+document.body.style.border = "5px solid red";
+
+var videoElement = document.querySelector("video");
+console.log(videoElement);
+
+
+/*
+Spacebar press:
+videoElement.pause();
+videoElement.play();
+
+
+R: //reloadsthe video element
+videoElement.load();
+
+
+*/
